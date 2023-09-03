3b5fb2998e8347f87fb6fcce16cb3ca426909796
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-09-03 11:08:06 -0300

    pagina inicial e primeiros components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Login from './pages/login/Login';
+import Register from './pages/Register/Register'
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 
 
 function App() {
@@ -17,6 +21,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
         <Footer />
